ff6a72509e38b50a9036dad68f834ef352b22345
Henrik Doark <henrikdoark@gmail.com>
2021-04-26 22:17:19 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+x = 5
+y = 2
+
+print(x)
+print(y)
+
+print('hello world')
+
+z = x + y
+print(z)
+
+cor_verde = 'verde'
+cor_vermelho = 'vermelho'
+
+ola = 'youtube '
+boa_noite = 'boa noite'
+print(ola + boa_noite)
