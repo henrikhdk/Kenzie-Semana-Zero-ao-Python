6ae2e8536b327da5081576598ec7c30257792dc6
Henrik Doark <henrikdoark@gmail.com>
2021-05-01 18:48:28 -0300

    Update do codigo de exemplo e familarização com o Python!

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
-x = 5
-y = 2
+# x = 5
+# y = 2
+#
+# print(x)
+# print(y)
+#
+# print('hello world')
+#
+# z = x + y
+# print(z)
+#
+# cor_verde = 'verde'
+# cor_vermelho = 'vermelho'
+#
+# ola = 'youtube '
+# boa_noite = 'boa noite'
+# print(ola + boa_noite)
+#
+#
+# A = False
+# B = True
+#
+# if A:
+#     print("A é maior que B")
+# elif B:
+#     print("B é maior que A")
+# else:
+#     print("B é igual a que A")
 
-print(x)
-print(y)
+mes = input("Qual mês é o seu aniversario?")
+dia = int(input("Qual o dia?"))
 
-print('hello world')
+if mes == 'jan':
+    tipo = "ground"
+elif mes == 'fev':
+    tipo = "fighting"
 
-z = x + y
-print(z)
+if dia == 1:
+    tipo = tipo + " electric"
 
-cor_verde = 'verde'
-cor_vermelho = 'vermelho'
-
-ola = 'youtube '
-boa_noite = 'boa noite'
-print(ola + boa_noite)
+print(tipo)
